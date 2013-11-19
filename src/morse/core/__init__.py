4eb5ce008f87d42407d90face9018ece07ab3941
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-11-19 23:05:21 +0100

    Allow to force a particular API
    Just set morse.core.api to an API class *before* calling morse.blender.main()

--- src/morse/core/__init__.py
+++ src/morse/core/__init__.py
@@ -1,9 +1,16 @@
-# running in Blender?
-try:
-    from morse.core.api_blender import BlenderAPI
-    blenderapi = BlenderAPI()
-except ImportError:
-    from morse.core.engineapi import EngineAPI
-    # Can fail if we are in Blender but not yet in the GameEngine,
-    # typically at 'Builder' stage.
-    blenderapi = EngineAPI()
+import logging; logger = logging.getLogger("morse." + __name__)
+
+# did some forced the API to use?
+if 'api' in globals():
+    logger.warn("Force loading of API <%s>" % api.__name__)
+    blenderapi = api()
+else: # try to auto-detect
+    try:
+        # running in Blender?
+        from morse.core.api_blender import BlenderAPI
+        blenderapi = BlenderAPI()
+    except ImportError:
+        from morse.core.engineapi import EngineAPI
+        # Can fail if we are in Blender but not yet in the GameEngine,
+        # typically at 'Builder' stage.
+        blenderapi = EngineAPI()
